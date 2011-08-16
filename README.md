30045903829d3f31e1d8263a5a7dc463bad92551
Dale Harvey <dale@arandomurl.com>
2011-08-16 17:04:19 +0100

    Added an initial empty application that starts Couchbase
    Change-Id: I13b051b25c41f6aa46ef81663424d72d540aa1ab
    Reviewed-on: http://review.couchbase.org/8992
    Reviewed-by: Dale Harvey <daleharvey@arandomurl.com>
    Tested-by: Dale Harvey <daleharvey@arandomurl.com>

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-Coming Soon
+## Empty App
+
+EmptyApp is minimal application that can be used to verify your installation works correctly.
+
+### Install
+
+    git clone git://github.com/couchbase/Android-EmptyApp.git
+    git clone git://github.com/couchbaselabs/Android-Couchbase.git
+
+And open both (`File -> Import -> Existing Projects`) projects in eclipse
+
+You will need to ensure EmptyApp has the correct path to Android-Couchbase, right click the EmptyApp project and select `Properties -> Android` and in the bottom right make sure to add the Android-Couchbase library.
+
+Now right click the EmptyApp project and select `Run As -> Android Application`, the installation should take around a minute and in the log you should see:
+
+    V/Couchbase( 1505): Apache CouchDB has started. Time to relax.
+    V/Couchbase( 1505): [info] [<0.41.0>] Apache CouchDB has started on http://0.0.0.0:34680/
+    V/EmptyApp( 1505): Couchbase has started
+    V/Couchbase( 1505): =PROGRESS REPORT==== 16-Aug-2011::16:47:38 ===
+    V/Couchbase( 1505):          application: couch
+    V/Couchbase( 1505):           started_at: nonode@nohost
---
+++ src/com/couchbase/emptyapp/EmptyAppActivity.java
@@ -0,0 +1,42 @@
+package com.couchbase.emptyapp;
+
+import android.app.Activity;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.couchbase.libcouch.CouchbaseEmbeddedServer;
+import com.couchbase.libcouch.ICouchClient;
+
+public class EmptyAppActivity extends Activity {
+
+	@SuppressWarnings("unused")
+	private ServiceConnection couchServiceConnection;
+
+	protected static final String TAG = "EmptyApp";
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+		CouchbaseEmbeddedServer couch = new CouchbaseEmbeddedServer(getBaseContext(), mCallback);
+		couchServiceConnection = couch.startCouchbase();
+    }
+
+	private final ICouchClient mCallback = new ICouchClient.Stub() {
+		@Override
+		public void couchStarted(String host, int port) {
+			Log.v(TAG, "Couchbase has started");
+		}
+
+		@Override
+		public void installing(int completed, int total) {
+		}
+
+		@Override
+		public void exit(String error) {
+		}
+	};
+}
